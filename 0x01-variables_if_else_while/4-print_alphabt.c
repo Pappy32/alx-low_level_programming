f8803e2bcf88b15f3d6e1511f41577d033908231
pappy32 <mawukogovina@gmail.com>
2023-02-20 11:04:12 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - a program that prints the alphabet in lowercase using putchar
- * Return: 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,11 +13,12 @@ int main(void)
 	e = 'e';
 	q = 'q';
 
-	for (low = 'a'; low <= 'z' low++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
 		if (low != e && low != q)
 			putchar(low);
 	}
-	putchar('\n');
+		printf("\n");
+
 	return (0);
 }
